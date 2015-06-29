875806c6bf8a29ec1fb7e982331c97de9ac67d90
Tim Whitacre <tim@timw.co>
2015-06-29 09:52:12 -0400

    Working on controllers

--- controllers/car.controller.go
+++ controllers/car.controller.go
@@ -1,17 +1,21 @@
 package controllers
 
 import (
-	"githu.com/gin-gonic/gin"
+	"github.com/twhitacre/go-cars/models"
 )
 
 func CarList() {
-
+  car := models.Car{}
+  return car
 }
 
 func CarNew() {
-
+  car := models.Car{}
+  return car
 }
 
 func CarSingle() {
-
+  car := models.Car{}
+  return car
 }
+
